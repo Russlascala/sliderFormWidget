9dd988e2b29d85a45ba8538d0841a04ccb6d4bf0
Russ Lascala <russlascala@gmail.com>
2018-09-20 16:54:02 -0400

    fixed the button to bring you to top of page

--- script.js
+++ script.js
@@ -99,15 +99,15 @@ $(document).ready(function () {
         alert('OOPS ERROR! This is just a demo site.');
         /* return false; */
     })
-
-    // When the user clicks on the button, scroll to the top of the document
-    function scrollToTop(scrollDuration) {
-        var scrollStep = -window.scrollY / (scrollDuration / 15),
-            scrollInterval = setInterval(function () {
-                if (window.scrollY != 0) {
-                    window.scrollBy(0, scrollStep);
-                }
-                else clearInterval(scrollInterval);
-            }, 15);
-    }
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function () {
+            if (window.scrollY != 0) {
+                window.scrollBy(0, scrollStep);
+            }
+            else clearInterval(scrollInterval);
+        }, 15);
+}
